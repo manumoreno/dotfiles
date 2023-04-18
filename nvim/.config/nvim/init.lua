3ec6e36c3dbd3260477d6d6535c2688e7522ed11
Manuel Moreno <manu_moreno@hotmail.com>
2023-04-18 09:20:22 -0500

    shuffle things around

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,24 +5,24 @@ require("custom.packer")
 require("custom.lsp_config")
 require("custom.completions")
 require("custom.treesitter")
-require("custom.bufferline")
+require("custom.bufferline")    -- functional. Has a separatator issue; due to terminal support?
 require("custom.trouble")
-require("custom.noice")
+require("custom.noice")         -- a bit noisy; tweak!
 
 -- non critical plugins
--- require("custom.tokyonight")
-require("custom.dap")
+require("custom.dap")           -- works but may need further tweaks
 require("custom.catppuccin")
 require("custom.lualine")
 require("custom.neotree")
 require("custom.gitsigns")
 require("custom.diagnostics")
-require("custom.autocommands")
+require("custom.autocommands")  -- osc52 plugin stuff needs cleanup
 require("custom.alpha")
 require("custom.toggleterm")
-require("custom.whitespace")
-require("custom.osc52")
+require("custom.osc52")         -- works great; especially over ssh sessions in tmux; needs cleanup
 require("custom.colorizer")
-require("custom.neorg")
--- require("custom.notebook")
---require("custom.wilder")
+require("custom.indent_blankline")
+require("custom.trail_whitespace")
+require("custom.neorg")         -- mappings not in place yet; in progress
+-- require("custom.notebook")   -- along with magma plugin
+--require("custom.wilder")      -- superseded by noice plugin and/or cmp
